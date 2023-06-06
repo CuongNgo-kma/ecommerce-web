c4ca959da318b65f53778ef5399670c8e4f6ec52
CuongNgo-kma <ngongocbiencuong@gmail.com>
2023-06-06 19:42:17 +0700

    .

--- FE/src/components/mainpages/auth/Login.js
+++ FE/src/components/mainpages/auth/Login.js
@@ -1,24 +1,25 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import url from '../../../api/url'
 function Login() {
   const [user, setUser] = useState({
     email: '', password: ''
   })
   const onChangeInput = e => {
     const { name, value } = e.target
-    setUser({ ...user, [name]: value.toLowerCase() })
+    setUser({ ...user, [name]: value })
   }
   const loginSubmit = async (e) => {
     e.preventDefault()
     try {
-      await axios.post('/user/login', { ...user })
+      await axios.post(`/user/login`, { ...user })
       localStorage.setItem('firstLogin', true)
       localStorage.setItem('email',user.email)
       window.location.href = "/"
 
     } catch (error) {
-      alert(error.response.data.msg)
+      alert(error)
     }
   }
   return (
